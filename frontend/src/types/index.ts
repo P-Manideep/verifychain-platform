6f73dcc572658d5d1596d87a412680d2e041402a
P-Manideep <pothkanmanideep@gmail.com>
2026-03-08 14:10:59 +0530

    add: Create fresh types folder

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export interface Document {
+  id: number;
+  filename: string;
+  file_hash: string;
+  file_size: number;
+  uploaded_at: string;
+  is_verified: boolean;
+  block_index: number;
+  block_hash: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
